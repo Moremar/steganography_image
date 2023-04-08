58594ed2ae85c165d2b1eef44b0cfd826252808a
Moremar <chevallier.th@gmail.com>
2023-04-08 17:08:24 +0900

    Add tests for encoding/decoding

---
+++ tests.py
@@ -0,0 +1,64 @@
+import unittest
+import os
+import stenography_image as steno
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_byte_to_fragments(self):
+        # 8 bits per byte used for encoding (fragments between 0 and 255)
+        self.assertEqual(steno.byte_to_fragments(4, 8), [4])
+        self.assertEqual(steno.byte_to_fragments(252, 8), [252])
+        # 4 bits per byte used for encoding (fragments between 0 and 15)
+        self.assertEqual(steno.byte_to_fragments(4, 4), [0, 4])
+        self.assertEqual(steno.byte_to_fragments(252, 4), [15, 12])
+        # 2 bits per byte used for encoding (fragments between 0 and 3)
+        self.assertEqual(steno.byte_to_fragments(4, 2), [0, 0, 1, 0])
+        self.assertEqual(steno.byte_to_fragments(252, 2), [3, 3, 3, 0])
+        # 1 bit per byte used for encoding (fragments between 0 and 1)
+        self.assertEqual(steno.byte_to_fragments(4, 1), [0, 0, 0, 0, 0, 1, 0, 0])
+        self.assertEqual(steno.byte_to_fragments(252, 1), [1, 1, 1, 1, 1, 1, 0, 0])
+
+    def test_fragments_to_byte(self):
+        # 8 bits per byte used for encoding (fragments between 0 and 255)
+        self.assertEqual(steno.fragments_to_byte([4], 8), 4)
+        self.assertEqual(steno.fragments_to_byte([252], 8), 252)
+        # 4 bits per byte used for encoding (fragments between 0 and 15)
+        self.assertEqual(steno.fragments_to_byte([0, 4], 4), 4)
+        self.assertEqual(steno.fragments_to_byte([15, 12], 4), 252)
+        # 2 bits per byte used for encoding (fragments between 0 and 3)
+        self.assertEqual(steno.fragments_to_byte([0, 0, 1, 0], 2), 4)
+        self.assertEqual(steno.fragments_to_byte([3, 3, 3, 0], 2), 252)
+        # 1 bit per byte used for encoding (fragments between 0 and 1)
+        self.assertEqual(steno.fragments_to_byte([0, 0, 0, 0, 0, 1, 0, 0], 1), 4)
+        self.assertEqual(steno.fragments_to_byte([1, 1, 1, 1, 1, 1, 0, 0], 1), 252)
+
+    def test_get_header_bytes(self):
+        # the header contains the size encoded on 4 bytes
+        self.assertEqual(steno.get_header_bytes(4), [0, 0, 0, 4])
+        self.assertEqual(steno.get_header_bytes(256 * 255), [0, 0, 255, 0])
+        self.assertEqual(steno.get_header_bytes(256 * 256 * 256 * 3 + 2), [3, 0, 0, 2])
+
+    def test_encode_decode(self):
+        message_url = 'test_data/grimm_fairy_tales.txt'
+        image_url = 'test_data/himeji.png'
+        encoded_image_url = 'test_data/himeji_encoded.png'
+        decoded_message_url = 'test_data/decoded_message.txt'
+        bits_per_byte = 4
+        # encode the message in an image
+        steno.encode_message(message_url, bits_per_byte, image_url, encoded_image_url)
+        # decode the message from that image
+        steno.decode_message(bits_per_byte, encoded_image_url, decoded_message_url)
+        # ensure the initial original message and the decoded message are equal
+        with open(message_url, 'r') as f1:
+            initial_message = f1.read()
+        with open(decoded_message_url, 'r') as f2:
+            decoded_message = f2.read()
+        self.assertEqual(initial_message, decoded_message)
+        # cleanup generated files
+        os.remove(encoded_image_url)
+        os.remove(decoded_message_url)
+
+
+if __name__ == '__main__':
+    unittest.main()
